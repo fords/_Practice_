258893886ddf7f144a11a930d2826babdba1362a
fords <fords@users.noreply.github.com>
2018-05-28 07:40:16 -0400

    Init_add

--- cats_in_hats.rb
+++ cats_in_hats.rb
@@ -1,3 +1,16 @@
+
+=begin 
+You have 100 cats.
+Your rules are simple: whenever you visit a cat, you toggle it's hat status (if it
+already has a hat, you remove it.. if it does not have a hat, you put one on).
+All of the cats start hat-less. You cycle through 100 rounds of visiting cats.
+In the 1st round, you visit every cat. 
+In the second round, you visit every other cat.
+In the nth round, you visit every nth cat.. until the 100th round, in which you only
+visit the 100th cat.
+At the end of 100 rounds, which cats have hats?
+=end
+
 def cats_in_hats
   index = 1  
   arr =[]
@@ -12,7 +25,7 @@ def cats_in_hats
   add_div = 2
   loop do 
     break if index == 101
-      #print div.to_s
+      #toggle the value 
       while div < 101 
         if arr[div] == false 
           arr[div] = true 
@@ -30,7 +43,7 @@ def cats_in_hats
   end 
   result = []
   i = 0 
-  while i < 101 
+  while i < 101   # Remove offset and return the index value of cat wearing a hat
     result.append(i+1) if arr[i+1] == true 
     i += 1 
   end 
