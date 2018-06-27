7d81c441ea895683a7bc0583899d25463a18fff9
fords <fords@users.noreply.github.com>
2018-06-26 23:58:25 -0400

    Init_add

---
+++ longestPalindrome.java
@@ -0,0 +1,49 @@
+class Solution {
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);  //if odd length
+            extendPalindrome(s, i, i+1); //if even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+}
+
+public class MainClass {
+    public static String stringToString(String input) {
+        if (input == null) {
+            return "null";
+        }
+        return Json.value(input).toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+            
+            String ret = new Solution().longestPalindrome(s);
+            
+            String out = (ret);
+            
+            System.out.print(out);
+        }
+    }
+}
